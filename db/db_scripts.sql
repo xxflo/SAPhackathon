5b8aadb4bd1524e520687ef361df70a4ce97b1fc
Tanya-Zhou <zhouty_a@hotmail.com>
2018-11-21 21:21:00 -0800

    Create db_scripts.sql

---
+++ db/db_scripts.sql
@@ -0,0 +1,13 @@
+create database hackathon;
+use hackathon;
+
+DROP TABLE users;
+CREATE TABLE users(name VARCHAR(128), pwd VARCHAR(64), email VARCHAR(256)); 
+
+TRUNCATE TABLE users;
+INSERT INTO users VALUES ('Abid', 'winwin', 'abid.salahi.esfahani@sap.com');
+INSERT INTO users VALUES ('Floris', 'winwin', 'floris.huang@sap.com');
+INSERT INTO users VALUES ('Heesu', 'winwin', 'hee.su.kim@sap.com');
+INSERT INTO users VALUES ('Tanya', 'winwin', 'tanya.zhou@sap.com');
+INSERT INTO users VALUES ('Peng', 'winwin', 'zhen.peng.zhu@sap.com');
+
