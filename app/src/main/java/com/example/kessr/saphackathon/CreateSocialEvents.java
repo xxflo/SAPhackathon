57738c16d1a5a5f74e453830bb70312897de5155
Hee Su <kessris@hotmail.com>
2018-11-22 02:39:16 -0800

    add event

--- app/src/main/java/com/example/kessr/saphackathon/CreateSocialEvents.java
+++ app/src/main/java/com/example/kessr/saphackathon/CreateSocialEvents.java
@@ -2,12 +2,20 @@ package com.example.kessr.saphackathon;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class CreateSocialEvents extends AppCompatActivity {
+    ImageButton imageButton;
+
+    public void addEventImage(View view){
+        imageButton.setImageResource(R.drawable.event);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_social_events);
+        imageButton = findViewById(R.id.addImage);
     }
 }
