e826ac97efc23c6b4a312df69266bef744af33ba
Zhen Peng Zhu <zhenpeng.zhu@hotmail.com>
2018-11-21 21:28:48 -0800

    Login page submit 1

--- app/src/main/java/com/example/kessr/saphackathon/MainActivity.java
+++ app/src/main/java/com/example/kessr/saphackathon/MainActivity.java
@@ -2,12 +2,35 @@ package com.example.kessr.saphackathon;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String userEmail = null;
+    private String userPassword = null;
+
+    public void clickFunction(View button){
+        this.userEmail = ((EditText)findViewById(R.id.emailText)).getText().toString();
+        this.userPassword = ((EditText)findViewById(R.id.passwordText)).getText().toString();
+
+        if(authenticateUser()){
+            Toast.makeText(this, "Login successful!", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, "Login Failed", Toast.LENGTH_SHORT).show();
+        }
+    }
+    //TODO
+    private boolean authenticateUser(){
+        return false;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
