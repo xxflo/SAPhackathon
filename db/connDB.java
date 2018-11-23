075b7dd24203768d5bef06a1b98da96f92c7cc55
i863232 <tanya.zhou@sap.com>
2018-11-22 17:57:55 -0800

    database connection

---
+++ db/connDB.java
@@ -0,0 +1,19 @@
+import java.sql.*;
+
+public class connDB {
+    public static void main(String args[]){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con=DriverManager.getConnection(
+            "jdbc:mysql://localhost:3306/hackathon","root","bobobo");
+
+            Statement stmt=con.createStatement();
+            ResultSet rs=stmt.executeQuery("select * from users");
+            while(rs.next())
+                System.out.println(rs.getString(1)+"  "+rs.getString(2)+"  "+rs.getString(3));
+            con.close();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
